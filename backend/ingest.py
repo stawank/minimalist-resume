6a7f25750ffdd7c56b7684254634bc2320f6640c
Stawank <stawank@gmail.com>
2026-03-03 22:03:29 +0100

    changes due to  embeddeding strategy change

--- backend/ingest.py
+++ backend/ingest.py
@@ -32,7 +32,11 @@ chunks = text_splitter.split_documents(documents)
 
 # 4. Create Embeddings (Math version of your text)
 print("Downloading embedding model (MiniLM)...")
-embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+model_kwargs = {'device': 'cpu'}
+encode_kwargs = {'normalize_embeddings': False}
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2", 
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs)
 
 # 5. Save to Vector Database
 print(f"Creating database at {DB_PATH}...")
